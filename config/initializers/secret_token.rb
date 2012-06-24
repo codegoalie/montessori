95460e196c8568460f3e1f512fa65c22364c3486
Chris Marshall <chris@chrismar035.com>
2012-06-24 01:16:11 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Montessori::Application.config.secret_token = '50f30e1d7467f38182e0f6290efef19c7a05e631aaeee5aa57b803d352c050bffd1f31cc34622cb7379c3c06b56b9d543b89c303650edf1fd44ed91de60fa8b2'
